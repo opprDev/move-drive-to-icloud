10e375d8d1a56110409ec554b8cad7a3ff7a31e5
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2025-11-10 01:32:23 +0000

    [UPDATE] Basic Source Code

--- scripts/move_drive_to_icloud.sh
+++ scripts/move_drive_to_icloud.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 # move_drive_to_icloud.sh
+# Moves files from Google Drive to iCloud on macOS
+# Author: Francisco Maria Calisto
+# Date: November 10, 2025
 
-SRC="$HOME/Google Drive/My Drive/YourFolder"
-DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/YourFolder"
+SRC="$HOME/Library/CloudStorage/GoogleDrive-francisco.mcalisto@gmail.com/My Drive"
+DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Desktop"
 
-echo "Moving files from Google Drive to iCloud..."
-mv "$SRC"/* "$DEST"/
+echo "DOING: Starting file transfer from Google Drive to iCloud..."
+echo "From: $SRC"
+echo "To:   $DEST"
 
-echo "Done."
+# Perform rsync
+rsync -av --remove-source-files "$SRC"/ "$DEST"/
+
+echo "CHECK: Transfer complete."
